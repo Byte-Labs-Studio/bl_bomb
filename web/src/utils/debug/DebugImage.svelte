f7c5a5fe478b642c82e1534ca66a15e56b2ab978
DevX32 <dishantacnc389@gmail.com>
2024-08-22 12:43:41 +0545

    fix(web): missing alt attribute

--- web/src/utils/debug/DebugImage.svelte
+++ web/src/utils/debug/DebugImage.svelte
@@ -1,3 +1,5 @@
-
-
-<img class="w-screen h-screen absolute top-0 left-0" src="https://i.imgur.com/602O0C8.png"/>
+<img 
+  class="w-screen h-screen absolute top-0 left-0" 
+  src="https://i.imgur.com/602O0C8.png" 
+  alt=""
+/>
